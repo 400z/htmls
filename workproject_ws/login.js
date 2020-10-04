14640217aadd330abb400e029dd34f26c41ef82b
original poster <68639438+400z@users.noreply.github.com>
2020-10-04 16:38:09 +0100

    Add files via upload

---
+++ workproject_ws/login.js
@@ -0,0 +1,37 @@
+function startTime() {
+    
+  var month = new Array();
+  month[0] = "Jan";
+  month[1] = "Feb";
+  month[2] = "Mar";
+  month[3] = "Apr";
+  month[4] = "May";
+  month[5] = "Jun";
+  month[6] = "Jul";
+  month[7] = "Aug";
+  month[8] = "Sep";
+  month[9] = "Oct";
+  month[10] = "Nov";
+  month[11] = "Dec";
+
+
+  
+  var today = new Date();
+    var a = today.getDay();
+    var b = month[today.getMonth()];
+    var c = today.getDate();
+    var h = today.getHours();
+    var m = today.getMinutes();
+    var s = today.getSeconds();
+    m = checkTime(m);
+    s = checkTime(s);
+    document.getElementById('daym').innerHTML = 
+    b + ", " + c;
+    document.getElementById('txt').innerHTML =
+    h + ":" + m + ":" + s;
+    var t = setTimeout(startTime, 500);
+  }
+  function checkTime(i) {
+    if (i < 10) {i = "0" + i};  // add zero in front of numbers < 10
+    return i;
+  }
